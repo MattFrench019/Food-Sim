9c9eaee5563d66ca5dda9406d0da0c0ed20b8dbc
MattFrench019 <57752799+MattFrench019@users.noreply.github.com>
2020-03-11 14:29:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='matts-food-sim',
-    version='1.0.0',
+    version='1.0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description="Python package for simulating a town's food consumption",
