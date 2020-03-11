13acbac496f9093e3061d2eb76e149de4fc7ddcf
MattFrench019 <57752799+mattfrench019@users.noreply.github.com>
2020-03-11 14:20:08 +0000

    PyPi

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='matts-food-sim',
-    version='1.0.0',
-    packages=find_packages(exclude=['tests*']),
+    version='1.0.1',
+    packages=find_packages(),
     license='MIT',
     description="Python package for simulating a town's food consumption",
     long_description=open('README.md').read(),
     install_requires=[],
     url='https://github.com/MattFrench019/Food-Sim',
     author='Matt French',
-    author_email='matt@mfrench.net'
+    author_email='matt@mfrench.net',
+    python_requires=">=3.6"
 )
